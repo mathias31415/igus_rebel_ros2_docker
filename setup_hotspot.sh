767175682d44fc85d839df9955170a2ded5f5bda
Robin Wolf <woro1021@h-ka.de>
2024-07-22 16:48:24 +0200

    new password

--- setup_hotspot.sh
+++ setup_hotspot.sh
@@ -2,7 +2,7 @@
 
 # Set the hotspot name and password
 HOTSPOT_NAME="AGV"
-HOTSPOT_PASSWORD="agv"
+HOTSPOT_PASSWORD="agv12345"
 HOTSPOT_INTERFACE="wlan0" # on PC: "wlp1s0" #on RPI5: "wlan0"
 
 # Create the hotspot
