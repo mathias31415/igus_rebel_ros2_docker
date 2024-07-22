ee48f415f64d49dbc5cb7aea586342631c235a19
Robin Wolf <woro1021@h-ka.de>
2024-07-22 16:23:16 +0200

    reboot button works in autostart

--- reboot_on_button.py
+++ reboot_on_button.py
@@ -1,19 +1,6 @@
-#import RPi.GPIO as GPIO
 from gpiozero import Button
 import os
 import time
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-# try:
-#     while True:
-#         if GPIO.input(18) == GPIO.LOW:
-#             os.system('sudo reboot')
-#             time.sleep(1)
-# except KeyboardInterrupt:
-#     GPIO.cleanup()
-
 
 button = Button(18)
 
