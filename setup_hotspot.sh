cbd76181292c4d7be49af587250ee50e1a77126b
Robin Wolf <woro1021@h-ka.de>
2024-07-22 16:46:33 +0200

    new hotspot name

--- setup_hotspot.sh
+++ setup_hotspot.sh
@@ -3,7 +3,7 @@
 # Set the hotspot name and password
 HOTSPOT_NAME="AGV"
 HOTSPOT_PASSWORD="agv"
-HOTSPOT_INTERFACE="wlp1s0" #on RPI5: "wlan0"
+HOTSPOT_INTERFACE="wlan0" # on PC: "wlp1s0" #on RPI5: "wlan0"
 
 # Create the hotspot
 nmcli device wifi hotspot ifname "$HOTSPOT_INTERFACE" ssid "$HOTSPOT_NAME" password "$HOTSPOT_PASSWORD"
